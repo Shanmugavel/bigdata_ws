167412b8f9cdb5a75c4173d00c8800517784a82e
Shan <shanmugavel.rs@gmail.com>
2015-11-23 17:28:31 -0700

    Added Second Boot Project!

---
+++ HelloActuator/src/main/java/xyz/shanmugavel/poc/boot/HelloActuator/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package xyz.shanmugavel.poc.boot.HelloActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shanmugavelsundaramoorthy
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
